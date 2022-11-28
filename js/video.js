857881ce9cea245b696a8d5432d2b79983571206
psyoo1996 <89875739+psyoo1996@users.noreply.github.com>
2022-11-27 23:58:51 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,1 +1,59 @@
-// Add js here.
+var vid = document.getElementById("video");
+
+document.getElementById("play").onclick = function() {
+    play()
+    };
+
+function play() {
+  vid.play();
+  vid.playbackRate = 1.0;
+}
+
+document.getElementById("pause").onclick = function() {
+    pause()
+    };
+
+function pause() {
+    vid.pause();
+  }
+
+document.getElementById("slower").onclick = function() {
+    slower()
+    };
+
+function slower() {
+    vid.playbackRate = 0.5;
+    alert("Video is at slowest speed!");
+}
+
+document.getElementById("faster").onclick = function() {
+    faster()
+    };
+
+function faster() {
+    vid.playbackRate = 2.0;
+    alert("Video is at fastest speed!");
+}
+
+document.getElementById("skip").onclick = function() {
+    skip()
+    };
+
+function skip() { 
+    vid.currentTime=15;
+  } 
+
+document.getElementById("mute").onclick = function() {
+    mute()
+    };
+
+function mute() { 
+    vid.muted = true;
+  } 
+
+
+
+
+
+
+
