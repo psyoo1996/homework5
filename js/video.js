b283b86081962deb41956bba0877c43c9a8c6fac
psyoo1996 <89875739+psyoo1996@users.noreply.github.com>
2022-11-29 15:42:07 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,8 +1,13 @@
-var vid = document.getElementById("video");
+var vid = document.getElementById("videoplayer");
+
+function disableLoop() {
+    vid.loop = false;
+    vid.load();
+}
 
 document.getElementById("play").onclick = function() {
     play()
-    };
+};
 
 function play() {
   vid.play();
@@ -11,48 +16,80 @@ function play() {
 
 document.getElementById("pause").onclick = function() {
     pause()
-    };
+};
 
 function pause() {
     vid.pause();
-  }
+}
 
 document.getElementById("slower").onclick = function() {
     slower()
-    };
+};
 
 function slower() {
-    vid.playbackRate = 0.5;
-    alert("Video is at slowest speed!");
+    if (vid.playbackRate == 0.5) {
+        alert("Video is at slowest speed!");
+    }
+    else if (vid.playbackRate == 1.0) {
+        vid.playbackRate = 0.5;
+    }
+    else if (vid.playbackRate == 2.0) {
+        vid.playbackRate = 1.0;
+    }
 }
 
 document.getElementById("faster").onclick = function() {
     faster()
-    };
+};
 
 function faster() {
-    vid.playbackRate = 2.0;
-    alert("Video is at fastest speed!");
+    if (vid.playbackRate == 0.5) {
+        vid.playbackRate = 1.0;
+    }
+    else if (vid.playbackRate == 1.0) {
+        vid.playbackRate = 2.0;
+    }
+    else if (vid.playbackRate == 2.0) {
+        alert("Video is at fastest speed!");
+    }
 }
 
 document.getElementById("skip").onclick = function() {
     skip()
-    };
+};
 
-function skip() { 
-    vid.currentTime=15;
-  } 
+function skip() {
+    if (vid.currentTime < 52) {
+        vid.currentTime += 15;
+    }
+    else if (vid.currentTime >= 52) {
+        vid.currentTime = 0;
+    }
+}
 
 document.getElementById("mute").onclick = function() {
     mute()
+};
+
+function mute() {
+    if (vid.muted == true) {
+        vid.muted = false;
+        document.getElementById("mute").innerHTML = "Mute";
+    }
+    else {
+        vid.muted = true;
+        document.getElementById("mute").innerHTML = "Unmute";
     };
+}
 
-function mute() { 
-    vid.muted = true;
-  } 
+document.getElementById("slider").onchange = function() {
+    slider()
+};
 
-
-
+function slider () {
+    vid.volume = document.getElementById("slider").value / 100;
+    document.getElementById("volume").innerHTML = document.getElementById("slider").value;
+}
 
 
 
